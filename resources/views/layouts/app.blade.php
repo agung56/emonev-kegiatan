65c3514f8c36c30943f4e1f2a7ceea925ce337aa
agung56 <50097116+agung56@users.noreply.github.com>
2026-03-12 22:00:38 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Monev KPU Kabupaten Pasuruan</title>
+    <title>E-Monev Kegiatan KPU Kabupaten Pasuruan</title>
     <link rel="icon" type="image/png" href="{{ asset('assets/logo-kpu.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
